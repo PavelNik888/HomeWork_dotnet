0b6944b9b6bc2aeea53d5a5fbdc8cf25ae1661b2
Pavel <dabdab@mail.ru>
2022-10-16 19:23:01 +0300

    вторая задача

---
+++ Test2/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+
+Console.WriteLine("max: " + max);
