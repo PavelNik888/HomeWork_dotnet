4c6ae76e686890968fa587da1aa6b8b988097899
Pavel <dabdab@mail.ru>
2022-10-16 19:41:57 +0300

    even or odd number

---
+++ Test3/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = int.Parse(Console.ReadLine());
+
+
+
+if (a % 2 == 0)
+{
+    Console.WriteLine(a + " явяется четным числом");
+}
+else
+{
+    Console.WriteLine(a + " явяется НЕчетным числом");
+}
+
