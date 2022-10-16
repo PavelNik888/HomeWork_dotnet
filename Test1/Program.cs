c415aa8314becd97090a74b2add12f231a50028d
Pavel <dabdab@mail.ru>
2022-10-16 18:28:19 +0300

    min max

---
+++ Test1/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int max = 0;
+int min = 0;
+
+if (a >b)
+{
+    max = a;
+    min = b;
+}
+else
+{
+    max = b;
+    min = a;
+}
+
+Console.WriteLine("min: " + min + "  max: " + max);
